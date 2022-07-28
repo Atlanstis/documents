ec97ebff140eb25e0b819da0779b7ec01ece6674
Gypsophila。 <futur_wxc@163.com>
2022-07-28 11:04:48 +0800

    chore: cz-git 配置修改

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,35 +1,7 @@
 // .commitlintrc.js
 /** @type {import('cz-git').UserConfig} */
 module.exports = {
-  rules: {
-    'body-leading-blank': [1, 'always'],
-    'body-max-line-length': [2, 'always', 100],
-    'footer-leading-blank': [1, 'always'],
-    'footer-max-line-length': [2, 'always', 100],
-    'header-max-length': [2, 'always', 100],
-    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
-    'subject-empty': [2, 'never'],
-    'subject-full-stop': [2, 'never', '.'],
-    'type-case': [2, 'always', 'lower-case'],
-    'type-empty': [2, 'never'],
-    'type-enum': [
-      2,
-      'always',
-      [
-        'build',
-        'chore',
-        'ci',
-        'docs',
-        'feat',
-        'fix',
-        'perf',
-        'refactor',
-        'revert',
-        'style',
-        'test',
-      ],
-    ],
-  },
+  extends: ['@commitlint/config-conventional'],
   prompt: {
     messages: {
       type: '选择你要提交的类型 :',
@@ -75,6 +47,6 @@ module.exports = {
       },
     ],
     skipQuestions: ['scope', 'body', 'breaking', 'footerPrefix', 'footer'],
-    maxHeaderLength: 80,
+    maxHeaderLength: 100,
   },
 };
